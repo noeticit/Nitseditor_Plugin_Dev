009fe0b230cdf1c0215f02ca5051d01fbff7363a
Nitish Kumar <nitishkumar@noeticitservices.com>
2019-01-07 21:54:18 +0530

    Few config enhancements

--- src/System/nitseditor.php
+++ src/System/nitseditor.php
@@ -1,9 +1,6 @@
 <?php
 
 return [
-    'packages' => [
-        //List of packages, this needs to be an array, format is prorperly available in read me/documentation.
-    ],
 
     /*
     * Application configurations
